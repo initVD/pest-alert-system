63ae534b941df5e79577ab5c50ddb6d0edbdcd54
initVD <vaidikpipaliya@gmail.com>
2025-09-26 15:19:00 +0530

    feat: Create Alert model and set up admin

--- alerts/models.py
+++ alerts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Alert(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    location = models.CharField(max_length=200)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
